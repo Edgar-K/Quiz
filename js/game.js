596538eb424ba661d427f6fdcb2891d75e633bee
Edgar Kocharyan <58892166+Edgar-K@users.noreply.github.com>
2020-05-13 22:38:27 +0200

    first commit

---
+++ js/game.js
@@ -0,0 +1,117 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [];
+
+fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple").then(res => {
+    return res.json();
+
+}).then(loadedQuestions => {
+    console.log(loadedQuestions.results);
+    questions = loadedQuestions.results.map(loadedQuestion => {
+        const formattedQuestion = {
+            question: loadedQuestion.question
+        };
+
+        const answerChoices = [...loadedQuestion.incorrect_answers];
+        formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+        answerChoices.splice(formattedQuestion.answer - 1, 0, loadedQuestion.correct_answer);
+
+        answerChoices.forEach((choice, index) => {
+            formattedQuestion["choice" + (index + 1)] = choice;
+        })
+
+        return formattedQuestion;
+    });
+
+
+
+    startGame();
+}).catch(err => {
+    console.log(err);
+});
+
+// Constants
+const Correct_Bonus = 10;
+const Max_Questions = 5;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+    //loader
+    game.classList.remove('hidden');
+    loader.classList.add("hidden");
+};
+
+getNewQuestion = () => {
+    // go to the new page after the answer all questions
+    if (availableQuestions.length === 0 || questionCounter >= Max_Questions) {
+        localStorage.setItem("mostRecentScore", score);
+        // go to the end of page
+        return window.location.assign("end.html")
+    }
+
+    questionCounter++;
+
+    progressText.innerText = `Question ${questionCounter}/${Max_Questions}`;
+
+    //Update the progress Bar
+
+    progressBarFull.style.width = ` ${(questionCounter / Max_Questions) * 100}%`;
+
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        const number = choice.dataset["number"];
+        choice.innerText = currentQuestion["choice" + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+
+        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+        if (classToApply === "correct") {
+            incrementScore(Correct_Bonus);
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+
+    });
+});
+
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+
+}
+
